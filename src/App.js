6f4eba751d3722cc871f125b31afc56671013b0b
Davron01~ <d.mirabdullayev@mail.ru>
2023-08-04 15:46:15 +0500

    start animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
+import { motion } from 'framer-motion'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Pvariants = {
+    hidden: {
+      x: -1000,
+      opacity: 0
+    },
+    visible: {
+      x: 0,
+      opacity: 1
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <motion.img src={logo} className="App-logo"
+          alt="logo"
+          animate={{ rotate: 360 }}
+          // initial={{}}
+          transition={{
+            // delay: 3,
+            duration: 2,
+            repeat: Infinity,
+            repeatDelay: 1,
+            repeatType: 'reverse',
+            type: 'tween',
+            ease: "easeInOut"
+          }}
+
+
+        />
+        <motion.p
+          initial={'hidden'}
+          animate={'visible'}
+          transition={{
+            delay: 0.5
+          }}
+          variants={Pvariants}
+
+        >
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </motion.p>
         <a
           className="App-link"
           href="https://reactjs.org"
